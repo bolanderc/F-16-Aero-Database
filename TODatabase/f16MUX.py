7d08f853ddca132287cbf79e4c1a020c29886b07
Christian Bolander <39311650+bolanderc@users.noreply.github.com>
2021-02-19 14:29:45 -0700

    MU Model Complete
    Preliminary design of MU geometry complete. Ready to begin generating a database.

--- TODatabase/f16MUX.py
+++ TODatabase/f16MUX.py
@@ -23,28 +23,35 @@ import multiprocessing
 # Input the json module to make displaying dictionaries more friendly
 import json
 
-#my_scene.display_wireframe(show_vortices=True)
-#my_scene.export_stl(filename="F16.stl")
-alpha = np.linspace(-np.deg2rad(30), np.deg2rad(30), 6)
-beta = np.linspace(-np.deg2rad(30), np.deg2rad(30), 6)
-d_e = np.linspace(-np.deg2rad(30), np.deg2rad(30), 6)
-d_a = np.linspace(-np.deg2rad(30), np.deg2rad(30), 6)
-d_r = np.linspace(-np.deg2rad(30), np.deg2rad(30), 6)
-p = np.linspace(-1.57, 1.57, 5)
-q = np.linspace(-1.57, 1.57, 5)
-r = np.linspace(-0.3925, 0.3925, 5)
-cases = list(itertools.product(alpha, beta, d_e, d_a, d_r, p, q, r))
+input_file = "F16_input.json"
+my_scene = MX.Scene(input_file)
+my_scene.display_wireframe(show_vortices=True)
+my_scene.export_stl(filename="F16.stl")
+alpha = np.linspace(-np.deg2rad(30), np.deg2rad(30), 2)
+beta = np.linspace(-np.deg2rad(30), np.deg2rad(30), 2)
+# d_e = np.linspace(-np.deg2rad(30), np.deg2rad(30), 6)
+# d_a = np.linspace(-np.deg2rad(30), np.deg2rad(30), 6)
+# d_r = np.linspace(-np.deg2rad(30), np.deg2rad(30), 6)
+# p = np.linspace(-1.57, 1.57, 5)
+# q = np.linspace(-1.57, 1.57, 5)
+# r = np.linspace(-0.3925, 0.3925, 5)
+# cases = list(itertools.product(alpha, beta, d_e, d_a, d_r, p, q, r))
+cases = list(itertools.product(alpha, beta))
 paramlist = []
-num_cases_per_core = round(len(cases)/multiprocessing.cpu_count())
+num_cores = 2
+num_cases_per_core = round(len(cases)/num_cores)
 i = 0
 while i < len(cases):
     paramlist.append(cases[i:i + num_cases_per_core])
     i += num_cases_per_core
+def initializer():
+    global my_scene
+    my_scene = MX.Scene(input_file)
 def database(params):
     print(multiprocessing.current_process())
     num_cases = len(params)
-    input_file = "F16_input.json"
-    my_scene = MX.Scene(input_file)
+    # input_file = "F16_input.json"
+    # my_scene = MX.Scene(input_file)
     CL = np.zeros(num_cases)
     CD = np.zeros(num_cases)
     CS = np.zeros(num_cases)
@@ -54,11 +61,11 @@ def database(params):
     for i in range(num_cases):
         alpha = params[i][0]
         beta = params[i][1]
-        d_e = params[i][2]
+        # d_e = params[i][2]
         my_scene.set_aircraft_state(state={"alpha" : alpha,
                                            "beta" : beta,
                                            "velocity" : 222.5211})
-        my_scene.set_aircraft_control_state(control_state={"elevator" : d_e})
+        # my_scene.set_aircraft_control_state(control_state={"elevator" : d_e})
         FM = my_scene.solve_forces()
         CL[i] = FM["F16"]["total"]["CL"]
         CD[i] = FM["F16"]["total"]["CD"]
@@ -68,16 +75,13 @@ def database(params):
         Cn[i] = FM["F16"]["total"]["Cn"]
     return [CL, CD, CS, Cl, Cm, Cn]
 
-#t0 = time.time()
-#for i in range(len(paramlist)):
-#            print(database(paramlist[i]))
-#t1 = time.time()
-#print(t1 - t0)
-#t0 = time.time()
-pool = multiprocessing.Pool()
-
-coeffs = pool.map(database, cases)
-pool.close()
-pool.join()
-#t1 = time.time()
-#print(t1 - t0)
+# t0 = time.time()
+# res = database(cases)
+# t1 = time.time()
+# print(t1 - t0)
+# t0 = time.time()
+# pool = multiprocessing.Pool(num_cores)
+# with multiprocessing.Pool(num_cores, initializer, ()) as pool:
+#     results = pool.map(database, paramlist)
+# t1 = time.time()
+# print(t1 - t0)
